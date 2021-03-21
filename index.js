2e1332e57dd0debc69302caf1b45d915923542e3
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2021-03-21 16:07:25 +0000

    modify contacts

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new Client({
 });
 client.connect();
 
-app.get('/', (req, res) => {
+app.get('/contacts', (req, res) => {
     client.query('SELECT * FROM salesforce.Contact', (err, data) => {
         res.json(data);
     });
