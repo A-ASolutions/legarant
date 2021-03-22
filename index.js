63e28baccdce93b600d9e679116052a12d703b84
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2021-03-22 01:26:12 +0000

    update

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const client = new Client({
 });
 client.connect();
 
-app.get('/contacts', async(req, res) => {
+app.get('/contacts', (req, res) => {
     try {
-        const allContacts = await client.query('SELECT * FROM salesforce.Contact');
+        const allContacts = client.query('SELECT * FROM salesforce.Contact');
         res.json(allContacts.rows);
     } catch (err) {
         console.error(err.message);
