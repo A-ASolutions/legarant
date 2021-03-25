e9d44404dd602fc3ac306bde39ade075145e8f8d
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2021-03-25 03:31:35 +0000

    create contacts

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const pg = require('pg');
 
-
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,41 +17,78 @@ const client = new pg.Client({
         rejectUnauthorized: false
     }
 });
+
 client.connect(err => {
     if (err) {
         console.error('connection error', err.stack)
     } else {
         console.log('connected')
+
     }
 })
 
 
-app.get('/', (req, res) => {
+
+
+
+app.get('/contacts', (req, res) => {
     try {
-        const allContacts = client.query('SELECT * FROM salesforce.contact');
-        console.log(allContacts.rows);
-        res.json(allContacts.rows);
+        let allcontacts = client.query('SELECT * FROM salesforce.contact').then((data) => {
+            console.log(data.rows);
+            res.json(data.rows);
+        });
 
     } catch (err) {
         console.error(err.message);
 
     }
 });
+
+
+
+
+
+
+
+// create a contact after checking if it already exists or not
+
+
 
 
 
 app.post('/contacts', (req, res) => {
 
     try {
-        const {
-            lastName
-        } = req.body;
-        const newContact = createContact = client.query('INSERT INTO salesforce.Contact (lastname)  VALUES ($1)', [lastName]);
-        res.json(newContact.rows[0]);
+        let email = req.body.email;
+        let lastname = req.body.lastname;
+        let firstname = req.body.firstname;
+        let phone = req.body.phone;
+
+        let createContact = client.query('SELECT sfid, id FROM salesforce.Contact WHERE email=$1', [email]).then((data) => {
+            if (data !== undefined) {
+                if (data.rowCount == 0) {
+                    createContact = client.query('INSERT INTO salesforce.Contact (email, lastname, firstname, phone)  VALUES ($1, $2, $3, $4)', [email, lastname, firstname, phone]).then((d) => {
+                        res.send("contact has been added successfully");
+                    });
+                } else {
+                    createContact = client.query('SELECT sfid, id FROM salesforce.Contact WHERE email = $1', [email]).then((data) => {
+                        res.json(data.rows[0].sfid);
+                    });
+                }
+            } else {
+                res.json(createContact.rows[0]);
+            }
+        });
     } catch (err) {
         console.error(err.message);
+
     }
+
 });
+
+
+
+
 
 
 
